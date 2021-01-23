0217f7c8b3ee9a2b359e027fb85ba4d8205e8287
Robin <robin.both94@googlemail.com>
2021-01-23 16:13:20 +0100

    Upgrade SQLite to PostgreSQL.

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def admin_only(function):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
